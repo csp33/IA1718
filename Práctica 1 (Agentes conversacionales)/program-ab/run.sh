fccfd74a777d72f1fc722be43642f6b1f6166132
Carlos <karlossanpa@gmail.com>
2018-03-12 19:46:00 +0100

    Actualizado program-ab

--- Práctica 1 (Agentes conversacionales)/program-ab/run.sh
+++ Práctica 1 (Agentes conversacionales)/program-ab/run.sh
@@ -1,5 +1,4 @@
-#/bin/bash
+#!/bin/bash
 # Run bot
-rm -rf bots/mybot/aimlif/*
-echo "aimlif eliminado"
+rm -fr bots/mybot/aimlif/*
 java -cp lib/Ab.jar Main bot=mybot action=chat trace=false morph=false
