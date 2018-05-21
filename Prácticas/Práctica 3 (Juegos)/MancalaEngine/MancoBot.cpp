bf3e4c109db7ac69e5b4e44c9786f7ffaf82211c
Carlos Sánchez Páez <karlossanpa@gmail.com>
2018-05-21 17:11:31 +0200

    Pequeño cambio.

--- Prácticas/Práctica 3 (Juegos)/MancalaEngine/MancoBot.cpp
+++ Prácticas/Práctica 3 (Juegos)/MancalaEngine/MancoBot.cpp
@@ -190,6 +190,8 @@ Move MancoBot::nextMove(const vector<Move> &adversary, const GameState &state) {
   Move movimiento = M_NONE;
 
   int max = MIN;
+  int alpha = MIN;
+  int beta = MAX;
 
 #if DEBUG
   cerr << "Numero de sucesores: " << sucesores.size() << endl;
@@ -197,7 +199,7 @@ Move MancoBot::nextMove(const vector<Move> &adversary, const GameState &state) {
 
   // Este bucle obtiene el máximo de los hijos (es la raíz)
   for (auto it = sucesores.begin(); it != sucesores.end(); ++it) {
-    it->heuristica = alphaBeta(it->estado, 0, MIN, MAX, false);
+    it->heuristica = alphaBeta(it->estado, 0, alpha, beta, false);
 #if DEBUG
     cerr << "Heuristica " << it->heuristica << " max " << max << endl;
 #endif
